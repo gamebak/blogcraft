e09c91d54f9bc6518cb960df0358e9c92f114f2b
gamebak <alexandru.cobuz@gmail.com>
2015-02-01 22:36:33 +0200

    -i

--- _set.php
+++ _set.php
@@ -5,30 +5,5 @@ require_once("class/o1db.php");
 $t = new template;
 $db = new o1db;
 
-$db->key('db', 'What-is-SSL-encryption', "<h1>What is SSL encryption</h1>
-    <h4>I will try to be brief about what is ssl encryption and how it works.</h4>
-    <p><strong>SSL or Secure Sockets Layer</strong></p>
-    <p>SSL is the standard security technology for establishing an encrypted link between a web server and a browser (client side). This link ensures that all<br/> 
-    data passed between the web server and browsers remain private and integral and that nobody will be capable of breaching it. </p>
-    <p>SSL is an industry standard and is used by millions of websites in the protection of their online transactions with their customers, because it's a safe way<br/> 
-    to transfer infromation from client side to the server side without any other interference from other networks. Even if a hacker will be able to read your traffic<br/> 
-    logs, he won't be able to decrypt your information because SSL certificate ensures that.</p>
-
-    <p>To be able to create an SSL encrpyted and secure connection a web server requires an SSL Certificate. </p>
-    
-    <p><strong>How SSL encryption works</strong> ( in 6 simple steps ):</p>
-    <ol>
-        <li>A browser will attempt to connect to a website secured with SSL.</li>
-        <li>Browser will request that the web server identify itself.</li>
-        <li>Server will sent to the browser a copy of its SSL Certificate.</li>
-        <li>Browser checks whether it trusts the SSL Certificate based on the information received. If so, it sends a message to the server.</li>
-        <li>The server sends back a digitally signed acknowledgement to start an SSL encrypted session.</li>
-        <li>SSL Encrypted data is shared between the client side ( browser ) and the server side. That's when https appears in your browser.</li>
-    </ol>
-
-    <p><strong>Note for webmasters:</strong></p>
-    <p>To enable strong encryption for your website visitors, I would suggest you choose an SSL Certificate that enables at least 128-bit minimum encryption.</p>
-
-    <p><strong>Privacy:</strong></p>
-    <p>We take user's privacy very seriously, that's why we wanted to discuss about SSL encryption and how it works.</p>");
+$db->key('db', 'What-is-SSL-encryption', "");
 ?>
--- class/dbExtension.php
+++ class/dbExtension.php
@@ -2,28 +2,84 @@
 
 class db extends o1db
 {
-	public $recentlimit = 20;
+	public 
+	/**
+	*	@var db database connector
+	*/
+	$db,
+	$recentLimit = 20;
+
+	/**
+	* Convert from key to normal text
+	*
+	* @param string        $articleKey article key name
+	*
+	* @return string
+	*/
+	public function decodeArticleKey($articleKey)
+	{
+		return str_replace('-',' ',$articleKey);
+	}
+
+	/**
+	* Convert from normal text to key
+	* @param string        $articleKey article key name
+	*
+	* @return string
+	*/
+	public function encodeArticleKey($articleKey)
+	{
+		return str_replace(' ','-',$articleKey);
+	}
+
+	/**
+	* Add new key to recent articles
+	* @param string        $articleKey article key name
+	*
+	* @return boolean|string
+	*/
+	public function recentAdd($articleKey)
+	{
+		// create first recent article
+		if(!$this->key_exists($this->db, 'recent'))
+		{
+			$newKey[] = $articleKey;
+		}
+		// append the new article to the existing list
+		else
+		{
+			$newKey = unserialize($this->key($this->db, 'recent'));
+			if(count($newKey) > $this->recentLimit)
+			{
+				$newKey[] = $articleKey;
+				unset($newKey[0]);
+				$newKey = array_values($newKey);
+			}
+		}
+
+		$this->key($this->db, 'recent', $newKey);
+
+		return true;
+	}
 
 	/**
 	* Get recent list of articles
-	*
-	* @param string        $db database connector
 	* @param string        $article article inside the database
 	*
 	* @return boolean|array
 	*/
-	public function recent($db, $article = false)
+	public function recent($article = false)
 	{
 		// no table
-		if(!$this->table_exists($db)) return false;
+		if(!$this->table_exists($this->db)) return false;
 
 		// get recent articles
 		if( !$article )
 		{
-			if( !$this->key_exists($db, 'recent') ) return false;
+			if( !$this->key_exists($this->db, 'recent') ) return false;
 
 			// time() => 'article name'
-			$list = unserialize( $this->key( $db,'recent' ) );
+			$list = unserialize( $this->key($this->db,'recent' ) );
 
 			// return parsed array
 			return $list;
@@ -33,33 +89,64 @@ class db extends o1db
 		if( is_string($article) )
 		{
 			// just create the new article
-			if( !$this->key_exists($db, 'recent') ) $this->key( $db, 'recent', $article);
+			if(!$this->key_exists($this->db, 'recent')) $this->key($this->db, 'recent', $article);
 
-			$list = unserialize($this->key($db, 'recent'));
+			$list = unserialize($this->key($this->db, 'recent'));
 			// append the article to the recent list
-			while( count($list) > $this->recentlimit ) )
+			while(count($list) > $this->recentlimit ))
 			{
-				unset($list[0]); $list = array_values($list);
+				unset($list[0]);
+				$list = array_values($list);
 			}
 			$list[] = $article;
 
 			// add new recent list
-			$this->key($db, 'recent', serialize($list) );
+			$this->key($this->db, 'recent', serialize($list));
 
 			return true;
 		}
 		
 	}
 	/*
-	* @param string        $db database connector
+	* Get recent list with article keys
 	*
-	* @return array|boolean Array with articles
+	* @return array|boolean Array with recent article keys
 	*/
-	public function getTotalList($db)
+	public function getRecentList()
 	{
-		if(!$this->table_exists($db) || !$this->key_exists($db, 'top')) return false;
+		if(!$this->table_exists($this->db) || !$this->key_exists($this->db, 'recent')) return false;
 
-		$list = unserialize($this->key($db, 'top'));
+		$list = unserialize($this->key($this->db, 'recent'));
+
+		return $list;
+	}
+	/**
+	* Parse recent list results, with text instead of keys
+	*
+	* @return boolean|string
+	*/
+	public function getRecentParsedList()
+	{
+		$list = $this->getRecentList();
+
+		// make sure list has elements
+		if(!$list) return false;
+		
+		for($i = 0; $i<count($list); $i++) $list[$i] = $this->decodeArticleKey($list[$i]);
+
+		return $list;
+	}
+
+	/*
+	* Get total stored article keys
+	*
+	* @return boolean|array Array with article keys
+	*/
+	public function getTotalList()
+	{
+		if(!$this->table_exists($this->db) || !$this->key_exists($this->db, 'top')) return false;
+
+		$list = unserialize($this->key($this->db, 'top'));
 
 		return $list;
 	}
@@ -71,25 +158,46 @@ class db extends o1db
 	*
 	* @return boolean 
 	*/
-	public function addArticleTotalList($db, $articleKey)
+	public function addArticleTotalList($articleKey)
 	{
-		if(!$this->table_exists($db)) return false;
+		if(!$this->table_exists($this->db)) return false;
 		
 		// create first key
-		if(!$this->key_exists($db, 'top'))
+		if(!$this->key_exists($this->db, 'top'))
 		{
 			$newKey[] = $articleKey;
 		}
 		else
 		{
 			// append new key
-			$newKey = $this->getTotalList($db);
+			$newKey = $this->getTotalList();
 			$newKey[] = $arrKeys;
 		}
 
-		$this->key($db, 'top', serialize($newKey));
+		$this->key($this->db, 'top', serialize($newKey));
 
 		return true;
+	}
+
+	/**
+	* @param string        $articleNameKey article name as a key
+	* @param string        $value article text
+	*
+	* @return boolean
+	*/
+	public function addArticle($articleNameKey, $articleText)
+	{
+		// save article
+		$this->key( $this->db, $articleNameKey, $articleText);
+		
+		// save publish time
+		$this->key( $this->db, $articleNameKey.'.publish', time());
+
+		// add to recent list our article
+		$this->recentAdd($articleNameKey);
+
+		// add the article to the total
+		$this->addArticleTotalList($articleNameKey);
 	}
 }
 
