03e23baa7c5bfd163c1d4b2ce4b930ead8ac562d
gamebak <alexandru.cobuz@gmail.com>
2015-02-02 03:27:22 +0200

    -f

--- _set.php
+++ _set.php
@@ -10,7 +10,7 @@ $dbExtension = new dbExtension;
 
 // Extension database connector
 $dbExtension->db = 'db';
-$dbExtension->->create_table('db');
+$dbExtension->create_table('db');
 
 $output = $dbExtension->addArticle('Coffee table history', '<h3>Nowadays the coffee table is present in almost every living room all around the globe.</h3>
         <p><strong>Coffee table history</strong></p>
